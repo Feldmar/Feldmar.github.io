2b30c4cd97c7ca99ffc33fbe8358fea582ff9196
Feldmar <89993722+Feldmar@users.noreply.github.com>
2021-11-04 21:11:58 +0300

    thjrej

---
+++ js/script.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+const tabcontent = document.querySelector('.tabcontent');
+const tabheaderItems = document.querySelector('.tabheader__items');
+
+tabcontent.forEach((tab) =>{
+  tabcontent.classList.add('hide')
+  tabcontent.classList.add('fade')
+})
+
+tabheaderItems.addEventListener('click', (e)=>)
+
+});
