6280f805e1aca5161891d668cbd730ff150402c3
Feldmar <89993722+Feldmar@users.noreply.github.com>
2021-11-07 00:45:25 +0300

    Add files via upload

---
+++ app/js/app.js
@@ -0,0 +1,55 @@
+// import $ from 'jquery'; window.jQuery = $; window.$ = $ // import jQuery module (npm i -D jquery)
+
+// require('~/app/libs/mmenu/js/jquery.mmenu.all.min.js') // import vendor jQuery plugin example (not module)
+
+document.addEventListener('DOMContentLoaded', () => {
+	
+	const images = [...document.querySelectorAll('.slider__image')]
+	const left = document.querySelector('#left');
+	const right = document.querySelector('#right');
+	const number = document.querySelector('.slider__number');
+
+	let el = 0;
+	function firstRender(params) {
+		images.forEach((el, i) => {
+			if (i === 0) {
+				el.classList.add('active')
+			}
+		})
+		number.textContent = el + 1
+	}
+	firstRender();
+
+	function changeSlide() {
+		images.forEach((el) => {
+			el.classList.remove('active')
+		})
+		images[el].classList.add('active')
+		number.textContent = el + 1
+	}
+
+	left.addEventListener('click', (event) => {
+		if (!event.isTrusted) return; //for webpack
+		console.log('left', el);
+		if (el === 0) {
+			el = images.length - 1
+		} else {
+			el = el - 1
+		}
+		changeSlide()
+	})
+
+	right.addEventListener('click', (event) => {
+		if (!event.isTrusted) return; //for webpack
+		console.log('right', el);
+		if (el === images.length - 1) el = 0
+		else el = el + 1
+		changeSlide()
+	})
+
+
+
+	document.getElementById('button').onclick = function (){alert("Здравствуйте, я из компании Орифлейм)")}
+
+
+})
